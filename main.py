cbba978c803b29ea07138bb4060d1894ab0a9df0
MagosDragoon29 <carpenter.evan29@gmail.com>
2024-03-19 17:13:03 -0400

    my solution for getting a word count

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 def main():
-
+    word_count = 0
+    words = []
     with open("books/frankenstein.txt") as f: 
-        file_contents = f.read()
-        print(file_contents)
+        global book
+        book = f.read()
+        words = book.split()
+        #print(words)
+    for word in words:
+        if word != " ":
+            word_count += 1
+    print(word_count)
+    
 
 
 main()
